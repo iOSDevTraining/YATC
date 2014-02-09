2a209b556daa526466da96b13175065633579f95
Jacob Krall <jacob@jkrall.net>
2014-02-09 17:22:38 -0500

    Use the Social framework to load a list of 100 tweets from the (first) user’s timeline.

--- YATC/TCViewController.m
+++ YATC/TCViewController.m
@@ -6,19 +6,104 @@
 //  Copyright (c) 2014 Jacob Krall. All rights reserved.
 //
 
+#import <Accounts/Accounts.h>
+#import <Social/Social.h>
 #import "TCViewController.h"
 
 @interface TCViewController ()
 
 @property NSArray *entries;
 
+// Twitter connection code shamelessly stolen from
+// https://dev.twitter.com/docs/ios/making-api-requests-slrequest
+@property (nonatomic) ACAccountStore *accountStore;
+
 @end
 
 @implementation TCViewController
 
+- (BOOL)userHasAccessToTwitter
+{
+    return [SLComposeViewController isAvailableForServiceType:SLServiceTypeTwitter];
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.entries = @[];
+    [self fetchTimelineForUser];
+}
+
+- (void)fetchTimelineForUser
+{
+    if (!self.accountStore) {
+        _accountStore = [[ACAccountStore alloc] init];
+    }
+    //  Step 0: Check that the user has local Twitter accounts
+    if ([self userHasAccessToTwitter]) {
+        //  Step 1:  Obtain access to the user's Twitter accounts
+        ACAccountType *twitterAccountType =
+        [self.accountStore accountTypeWithAccountTypeIdentifier:
+         ACAccountTypeIdentifierTwitter];
+
+        [self.accountStore
+         requestAccessToAccountsWithType:twitterAccountType
+         options:nil
+         completion:^(BOOL granted, NSError *error) {
+             if (granted) {
+                 //  Step 2:  Create a request
+                 NSArray *twitterAccounts =
+                 [self.accountStore accountsWithAccountType:twitterAccountType];
+                 NSURL *url = [NSURL URLWithString:@"https://api.twitter.com"
+                               @"/1.1/statuses/home_timeline.json"];
+                 NSDictionary *params = @{@"include_rts" : @"0",
+                                          @"trim_user" : @"1",
+                                          @"count" : @"100"};
+                 SLRequest *request =
+                 [SLRequest requestForServiceType:SLServiceTypeTwitter
+                                    requestMethod:SLRequestMethodGET
+                                              URL:url
+                                       parameters:params];
+                 //  Attach an account to the request
+                 [request setAccount:[twitterAccounts lastObject]];
+
+                 //  Step 3:  Execute the request
+                 [request performRequestWithHandler:
+                  ^(NSData *responseData,
+                    NSHTTPURLResponse *urlResponse,
+                    NSError *error) {
+                      if (responseData) {
+                          if (urlResponse.statusCode >= 200 &&
+                              urlResponse.statusCode < 300) {
+                              NSError *jsonError;
+                              NSDictionary *timelineData =
+                              [NSJSONSerialization
+                               JSONObjectWithData:responseData
+                               options:NSJSONReadingAllowFragments error:&jsonError];
+                              if (timelineData) {
+                                  NSLog(@"Timeline Response: %@\n", timelineData);
+                              }
+                              else {
+                                  // Our JSON deserialization went awry
+                                  NSLog(@"JSON Error: %@", [jsonError localizedDescription]);
+                              }
+                          }
+                          else {
+                              // The server did not respond ... were we rate-limited?
+                              NSLog(@"The response status code is %d",
+                                    urlResponse.statusCode);
+                          }
+                      }
+                  }];
+             }
+             else {
+                 // Access was not granted, or an error occurred
+                 NSLog(@"%@", [error localizedDescription]);
+             }
+         }];
+    } else {
+        NSLog(@"!userHasAccessToTwitter");
+    }
 }
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
@@ -28,7 +113,7 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 4; // self.entries.count;
+    return self.entries.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
